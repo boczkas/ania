97379170c57efad175df05b42677fc47f5f30c2a
Przemek <przemyslawjakubowski@gmail.com>
2020-10-29 13:15:47 +0100

    Ciag Fibonacciego

---
+++ src/Fibonacci.java
@@ -0,0 +1,6 @@
+public class Fibonacci {
+    public static void main(String[] args) {
+        // https://pl.wikipedia.org/wiki/Ci%C4%85g_Fibonacciego
+        // Napisz program, ktory wypisze wartosc wyrazu z ciagu
+    }
+}
