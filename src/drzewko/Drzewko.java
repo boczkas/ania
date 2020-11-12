f90315cd6bd6b0c873e591bb2d9dbc2b243ea3bc
Przemek <przemyslawjakubowski@gmail.com>
2020-11-12 11:26:58 +0100

    drzewko rozwiazanie

--- src/drzewko/Drzewko.java
+++ src/drzewko/Drzewko.java
@@ -1,5 +1,19 @@
 package drzewko;
 
+import java.util.Scanner;
+
 public class Drzewko {
-    // narysuj drzewko swiateczne
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Jak wysokie ma byc drzewko?");
+
+        int wysokosc = scanner.nextInt();
+        for (int i = 0; i < wysokosc; i++) {
+            for (int j = 0; j < wysokosc - i; j++)
+                System.out.print(" ");
+            for (int k = 0; k < (2 * i + 1); k++)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
 }
