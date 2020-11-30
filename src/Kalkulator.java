de78c1ed117b37a1831787d6d0cc9874d0839753
GdanskaProgramistka <emug@vp.pl>
2020-11-30 20:30:09 +0100

    Dodaję Kalkulator z funkcją q

--- src/Kalkulator.java
+++ src/Kalkulator.java
@@ -7,27 +7,36 @@ public class Kalkulator {
         Scanner in = new Scanner(System.in);
         System.out.println("Podaj dwie liczby na których chesz wykonać działanie");
         double a = in.nextDouble();
+        String aString = String.valueOf(a);
         double b = in.nextDouble();
+        String bString = String.valueOf(b);
 
         System.out.println("Podaj jakie działanie chcesz przeprowadzić ");
-        String znak = in.next();
+        String operator = in.next();
+        String q = "q";
 
+        while (aString != q || bString != q || operator != q){
+            if (operator.equals("+")) {
+                double wynik = a + b;
+                System.out.println(a + " " + operator + " " + b + " = " + wynik);
+            } else if (operator.equals("-")) {
+                double wynik = a - b;
+                System.out.println(a + " " + operator + " " + b + " = " + wynik);
+            } else if (operator.equals("*")) {
+                double wynik = a * b;
+                System.out.println(a + " " + operator + " " + b + " = " + wynik);
+            } else if (operator.equals("/")) {
+                double wynik = a / b;
+                System.out.println(a + " " + operator + " " + b + " = " + wynik);
+            }
+            a = in.nextDouble();
+            b = in.nextDouble();
+            operator = in.next();
+        }
 
-        if (znak.equals("+")) {
-            double wynik = a + b;
-            System.out.println(a + " " + znak + " " + b + " = " + wynik);
-        }
-        else if (znak.equals("-")) {
-            double wynik = a - b;
-            System.out.println(a + " " + znak + " " + b + " = " + wynik);
-        }
-        else if (znak.equals("*")) {
-            double wynik = a * b;
-            System.out.println(a + " " + znak + " " + b + " = " + wynik);
-        }
-        else if (znak.equals("/")) {
-            double wynik = a / b;
-            System.out.println(a + " " + znak + " " + b + " = " + wynik);
+        while (aString == q || bString == q || operator == q){
+            System.out.println("Koniec pracy kalkulatora");
+            break;
         }
     }
 }
---
+++ src/Metody.java
@@ -0,0 +1,11 @@
+public class Metody {
+
+    String str(double a, double b){
+        System.out.println("Cokolwiek");
+        return "cokolwiek";
+    }
+
+    int liczba(String a, String b){
+        return 5;
+    }
+}
