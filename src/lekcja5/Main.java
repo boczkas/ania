f21bebb0a341917b7663233ca2ef940036e26adc
Przemek <przemyslawjakubowski@gmail.com>
2020-12-10 10:45:39 +0100

    Lekcja 5

---
+++ src/lekcja5/Main.java
@@ -0,0 +1,66 @@
+package lekcja5;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+//        1. Pusty łańcuch
+//        String pusty = "";
+//
+//        System.out.println(pusty.length() == 0);
+//        System.out.println(pusty.isEmpty());
+
+//        2. charAt
+//        String witaj = "elo";
+//        System.out.println(witaj);
+//
+//        char znak = witaj.charAt(1);
+//        System.out.println(znak);
+
+//        3. String API
+//        jak korzystać z tego co daje nam String
+
+//        4. Wejście/wyjście I/O
+
+//      Wyjśćie
+//        System.out.println("No elo");
+
+//      Wejście
+//        Scanner in = new Scanner(System.in);
+//        in.nextInt();
+//        in.nextDouble();
+
+//      5. Zapis i odczyt plików
+
+//        Odczyt
+
+//        Path fileName = Path.of(System.getProperty("user.dir") + "/src/lekcja5/test.txt");
+//        String text = Files.readString(fileName);
+//        System.out.println(text);
+
+//         Zapis
+//        FileWriter fileWriter = new FileWriter("eloszka.txt");
+//        fileWriter.write("Bardzo miło mi Panią powitać");
+//        fileWriter.close();
+
+        //        System.out.println(System.getProperty("user.dir"));
+
+//        6. Bloki i zasięgi
+
+//        {
+//            int n = 5;
+//
+//            {
+////                int n =5;
+//            }
+//        }
+//
+////        int n = 5;
+////        n = 10;
+//
+    }
+}
---
+++ src/lekcja5/Zadanie.java
@@ -0,0 +1,8 @@
+package lekcja5;
+
+public class Zadanie {
+    public static void main(String[] args) {
+//        Wyniki Mikołajkowego głosowania mają się znaleźć w plikach.
+//        Każdy plik ma nazywać się tak jak adresat wiadomości
+    }
+}
